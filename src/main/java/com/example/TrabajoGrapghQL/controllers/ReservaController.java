57457efcfafa9907117ca15a079697708894e560
Eloy Sánchez Tamayo <esanctam@myuax.com>
2024-01-07 16:24:29 +0100

    creacion metodos reservas

--- src/main/java/com/example/TrabajoGrapghQL/controllers/ReservaController.java
+++ src/main/java/com/example/TrabajoGrapghQL/controllers/ReservaController.java
@@ -4,6 +4,8 @@ import com.example.TrabajoGrapghQL.domain.Libro;
 import com.example.TrabajoGrapghQL.domain.Reserva;
 import com.example.TrabajoGrapghQL.repositories.LibroRepository;
 import com.example.TrabajoGrapghQL.repositories.ReservaRepository;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -18,7 +20,30 @@ public class ReservaController {
     }
 
     @QueryMapping
+    Iterable<Reserva> buscarReservaPorLibro(@Argument Long libroID) {
+        Libro libro = libroRepository.findById(libroID).orElseThrow(() -> new RuntimeException("Libro no encontrado"));
+        return reservaRepository.findByLibro(libro);
+    }
+
+    @QueryMapping
     Iterable<Reserva> listarReservas() {
         return reservaRepository.findAll();
+    }
+
+    @MutationMapping
+    Reserva agregarReserva(@Argument Long libroID) {
+        Reserva reserva = new Reserva();
+        reserva.setLibro(libroRepository.findById(libroID).orElseThrow(() -> new RuntimeException("Libro no encontrado")));
+        return reservaRepository.save(reserva);
+    }
+
+    @MutationMapping
+    Boolean eliminarReserva(@Argument Long id) {
+        if (reservaRepository.existsById(id)) {
+            reservaRepository.deleteById(id);
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- src/main/java/com/example/TrabajoGrapghQL/repositories/ReservaRepository.java
+++ src/main/java/com/example/TrabajoGrapghQL/repositories/ReservaRepository.java
@@ -1,9 +1,11 @@
 package com.example.TrabajoGrapghQL.repositories;
 
+import com.example.TrabajoGrapghQL.domain.Libro;
 import com.example.TrabajoGrapghQL.domain.Reserva;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReservaRepository extends JpaRepository<Reserva, Long> {
+    Iterable<Reserva> findByLibro(Libro libro);
 }
