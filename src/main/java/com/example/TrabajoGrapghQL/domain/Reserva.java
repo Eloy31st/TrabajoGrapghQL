6dc45c1c53bb445a49329d45a39526217ec11ce5
Eloy Sánchez Tamayo <esanctam@myuax.com>
2024-01-07 15:01:16 +0100

    creacion reserva

---
+++ src/main/java/com/example/TrabajoGrapghQL/domain/Reserva.java
@@ -0,0 +1,14 @@
+package com.example.TrabajoGrapghQL.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Reserva {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private Libro Libro;
+}
